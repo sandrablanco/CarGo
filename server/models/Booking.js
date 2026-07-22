f61772381f5b842d995de4eebc3be8c4e03da13c
Sandra Blanco <sandra.blanco18@gmail.com>
2026-07-22 17:52:32 +0200

    models/Booking.js

---
+++ server/models/Booking.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    car: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Car",
+        required: true,
+    },
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    endDate: {
+        type: Date,
+        required: true,
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["pending", "confirmed", "cancelled"],
+        default: "pending",
+    },
+},
+    {
+        timestamps: true,
+    }
+);      
+
+export default mongoose.model("Booking", userSchema);
