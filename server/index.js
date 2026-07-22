73d9fad3fcf39060cf6f8180d442aaeb982a54a1
Sandra Blanco <sandra.blanco18@gmail.com>
2026-07-22 17:08:39 +0200

    Convert server form submodule to regular folder

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB(); 
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("CarGo API IS RUNNING 🚗");
+});
+
+const PORT = process.env.PORT || 3000;  
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+
+});
+
+export default app;
