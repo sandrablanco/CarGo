3f5e8ce965637fee7cb62f27ccb15a8525fff9a3
Sandra Blanco <sandra.blanco18@gmail.com>
2026-07-24 19:13:00 +0200

    authRoutes.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import carRoutes from "./routes/carRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/cars", carRoutes);
+app.use("/api/auth", authRoutes);
 
 
 app.get("/", (req, res) => {
--- server/models/User.js
+++ server/models/User.js
@@ -42,7 +42,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         trim: true,
-    }
+    },
     postcode:{
         type: String,
         required: true,
