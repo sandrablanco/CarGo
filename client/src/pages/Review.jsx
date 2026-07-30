c1520a2b234a4a1ad2535be00260aa0da63d016b
Sandra Blanco <sandra.blanco18@gmail.com>
2026-07-30 16:25:18 +0200

    adding pages/Review.jsx

---
+++ client/src/pages/Review.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+
+function Review() {
+  const [rating, setRating] = useState(5);
+  const [comment, setComment] = useState("");
+  const [suggestion, setSuggestion] = useState("");
+  const [recommend, setRecommend] = useState("");
+  const [bookingExperience, setBookingExperience] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log({
+      rating,
+      comment,
+    });
+
+    alert("¡Gracias por valorar tu experiencia!");
+  };
+
+  return (
+    <div>
+      <h1>⭐ Valora tu experiencia</h1>
+
+      <form onSubmit={handleSubmit}>
+
+        <label>Puntuación</label>
+        <br />
+
+        <select
+          value={rating}
+          onChange={(e) => setRating(Number(e.target.value))}
+        >
+          <option value="5">⭐⭐⭐⭐⭐ (5)</option>
+          <option value="4">⭐⭐⭐⭐ (4)</option>
+          <option value="3">⭐⭐⭐ (3)</option>
+          <option value="2">⭐⭐ (2)</option>
+          <option value="1">⭐ (1)</option>
+        </select>
+
+        <br /><br />
+
+        <label>Comentario</label>
+        <br />
+
+        <textarea
+          rows="5"
+          cols="40"
+          placeholder="¿Qué te ha parecido el coche?"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+
+        <textarea
+          rows="5"
+          cols="40"
+          placeholder="¿Cómo fue su experiencia durante el proceso de reserva?"
+          value={bookingExperience}
+          onChange={(e) => setBookingExperience(e.target.value)}
+        />
+        
+        <textarea
+          rows="5"
+          cols="40"
+          placeholder="¿Desea sugerirnos alguna mejora?"
+          value={suggestion}
+          onChange={(e) => setSuggestion(e.target.value)}
+        />
+
+         <textarea
+          rows="5"
+          cols="40"
+          placeholder="¿Nos recomendaría a algún amigo o familiar?"
+          value={recommend}
+          onChange={(e) => setRecommend(e.target.value)}
+        />
+        <br /><br />
+
+        <button type="submit">
+          Enviar valoración 
+        </button>
+
+      </form>
+    </div>
+  );
+}
+
+export default Review;
